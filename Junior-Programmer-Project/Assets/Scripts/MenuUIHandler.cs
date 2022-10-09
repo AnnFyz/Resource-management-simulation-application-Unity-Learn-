1061b9a0125d1048b2bdd236cb6f48947deffbe5
AnnFyz <annfyz1232@yandex.ru>
2022-10-09 03:44:44 +0200

    add data persistence between sessions

--- Junior-Programmer-Project/Assets/Scripts/MenuUIHandler.cs
+++ Junior-Programmer-Project/Assets/Scripts/MenuUIHandler.cs
@@ -17,14 +17,15 @@ public class MenuUIHandler : MonoBehaviour
     public void NewColorSelected(Color color)
     {
         // add code here to handle when a color is selected
-       MainManager.Instance.TeamColor = color;
+        MainManager.Instance.TeamColor = color;
     }
-    
+
     private void Start()
     {
         ColorPicker.Init();
         //this will call the NewColorSelected function when the color picker have a color button clicked.
         ColorPicker.onColorChanged += NewColorSelected;
+        ColorPicker.SelectColor(MainManager.Instance.TeamColor); //This line will pre-select the saved color in the MainManager (if there is one) when the menu screen is launched.
     }
 
     public void StartNew()
@@ -34,9 +35,22 @@ public class MenuUIHandler : MonoBehaviour
 
     public void Exit()
     {
-      #if UNITY_EDITOR
+        MainManager.Instance.SaveColor();
+
+#if UNITY_EDITOR
         EditorApplication.ExitPlaymode();
-     #endif
+#endif
         Application.Quit();
+    }
+
+    public void SaveColorClicked()
+    {
+        MainManager.Instance.SaveColor();
+    }
+
+    public void LoadColorClicked()
+    {
+        MainManager.Instance.LoadColor();
+        ColorPicker.SelectColor(MainManager.Instance.TeamColor);
     }
 }
